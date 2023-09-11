114e27bfc8814624d7841aab4c4750ddc843ed51
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-09-11 02:40:16 -1100

    fix: styling

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -14,7 +14,7 @@ const MovieCard = () => {
         USA, 2018 - current
       </span>
       <h4 className="md:text-xl text-base font-bold">Stranger Things</h4>
-      <div className="flex items-center gap-1 md:gap-2">
+      <div className="flex items-center gap-1 justify-between">
         <span className="flex max-md:text-xs font-semibold items-center gap-1 md:gap-2">
           <Image src="images/tmdb.svg" alt="imdb" width={35} height={17} />
           860 / 100
