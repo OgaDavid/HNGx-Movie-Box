af72f31ca486875307e1e704d294e93f640fb11c
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-09-13 01:25:07 -1100

    fix: sidebar links

--- app/(routes)/movies/components/sidebar.tsx
+++ app/(routes)/movies/components/sidebar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Logo from "@/components/Navbar/logo";
 import { Button } from "@/components/ui/button";
@@ -11,22 +11,18 @@ import { cn } from "@/lib/utils";
 const sideLinks = [
   {
     name: "Home",
-    href: "/",
     icon: <Home />,
   },
   {
     name: "Movies",
-    href: "/movies",
     icon: <Clapperboard />,
   },
   {
     name: "TV Series",
-    href: "/movies/tv",
     icon: <Tv />,
   },
   {
     name: "Upcoming",
-    href: "/movies/upcoming",
     icon: <Calendar />,
   },
 ];
@@ -42,18 +38,17 @@ const Sidebar = () => {
         <ul className="flex my-16 flex-col gap-6 items-start">
           {sideLinks.map((link, index) => (
             <li key={index}>
-              <Link
+              <span
                 className={cn(
-                  "flex py-4 hover:border-r-rose-700 text-[#333333] hover:border-r-[6px] w-[226px] items-start gap-4 px-4 hover:bg-rose-100",
-                  pathname === link.href
+                  "flex py-4 hover:border-r-rose-700 cursor-pointer text-[#333333] hover:border-r-[6px] w-[226px] items-start gap-4 px-4 hover:bg-rose-100",
+                  link.name === "Movies"
                     ? "border-r-rose-700 bg-rose-100 border-r-[6px]"
                     : ""
                 )}
-                href={link.href}
               >
                 <span className="w-4 h-4">{link.icon}</span>
                 <span className="text-black font-bold ">{link.name}</span>
-              </Link>
+              </span>
             </li>
           ))}
         </ul>
